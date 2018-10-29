a52c1507b46005372141ec8a598bd8d92ef3d2da
Laura Nagy <ngylaura@gmail.com>
2018-10-29 16:32:13 +0000

    sets up FilmBox

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FilmBox extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Upcoming Film Releases for UK</h1>
+      </div>
+    )
+  }
+}
+
+export default FilmBox;
