36543ccc40a4999c4a5fd5817b42e320f2c87dd3
Laura Nagy <ngylaura@gmail.com>
2018-10-29 16:44:56 +0000

    sets up folder structure

--- src/containers/FilmBox.js
+++ src/containers/FilmBox.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import FilmList from '../components/FilmList';
+import UpcomingReleases from '../components/UpcomingReleases';
 
 class FilmBox extends Component {
   render() {
     return (
       <div>
         <h1>Upcoming Film Releases for UK</h1>
+        <FilmList />
+        <UpcomingReleases />
       </div>
     )
   }
