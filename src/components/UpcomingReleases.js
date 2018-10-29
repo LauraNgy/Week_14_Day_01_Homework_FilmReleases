08498ca90ace622ef3cdd736a42a457b61406481
Laura Nagy <ngylaura@gmail.com>
2018-10-29 17:12:06 +0000

    starts dynamic rendering

--- src/components/UpcomingReleases.js
+++ src/components/UpcomingReleases.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class UpcomingReleases extends Component {
   render() {
     return(
-      <h2><a href="https://www.imdb.com/calendar/?region=gb">View More Upcoming Releases >> </a></h2>
+      <h2><a href={this.props.calendar}>View More Upcoming Releases >> </a></h2>
     )
   }
 }
--- src/containers/FilmBox.js
+++ src/containers/FilmBox.js
@@ -3,12 +3,47 @@ import FilmList from '../components/FilmList';
 import UpcomingReleases from '../components/UpcomingReleases';
 
 class FilmBox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+              {
+                id: 1,
+                name: "Spider-Man: Into the Spider-Verse",
+                url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
+              },
+              {
+                id: 2,
+                name: "Life Itself",
+                url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+              },
+              {
+                id: 3,
+                name: "Mary Queen of Scots",
+                url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+              },
+              {
+                id: 4,
+                name: "The Lego Movie 2: The Second Part",
+                url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+              },
+              {
+                id: 5,
+                name: "Captain Marvel",
+                url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+              }
+      ],
+      calendar: "https://www.imdb.com/calendar/?region=gb"
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Upcoming Film Releases for UK</h1>
-        <FilmList />
-        <UpcomingReleases />
+        <FilmList data={this.state.data}/>
+        <UpcomingReleases calendar={this.state.calendar}/>
       </div>
     )
   }
