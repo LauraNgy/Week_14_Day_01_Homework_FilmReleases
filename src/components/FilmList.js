f3eee117fd8857cd0f287eaa5618b75f810fed55
Laura Nagy <ngylaura@gmail.com>
2018-10-29 16:54:14 +0000

    statically renders UpcomingReleases

---
+++ src/components/FilmList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class FilmList extends Component {
+
+  render() {
+    return (
+      <div>
+        I are Film List!
+      </div>
+    )
+  }
+}
+
+export default FilmList;
---
+++ src/components/UpcomingReleases.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class UpcomingReleases extends Component {
+  render() {
+    return(
+      <h2><a href="https://www.imdb.com/calendar/?region=gb">View More Upcoming Releases >> </a></h2>
+    )
+  }
+}
+
+export default UpcomingReleases;
